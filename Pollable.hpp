07b977cbc02a50e161d78251af9b022c090034ca
bobfogg <bob.fogg@celltracktech.com>
2019-04-08 14:41:34 -0400

    missing scope scope declarations,  string.h include

--- Pollable.hpp
+++ Pollable.hpp
@@ -23,7 +23,7 @@ using boost::static_pointer_cast;
 #include "VampAlsaHost.hpp"
 
 class Pollable;
-typedef std::map < std::string, shared_ptr < Pollable > > PollableSet;
+typedef std::map < std::string, boost::shared_ptr < Pollable > > PollableSet;
 
 class Pollable {
 public:
@@ -35,7 +35,7 @@ public:
   static PollableSet pollables; // map of Pollables, indexed by label, values are shared pointers
   static void remove(const string label);
   static Pollable * lookupByName (const std::string& label);
-  static shared_ptr < Pollable > lookupByNameShared (const std::string& label);
+  static boost::shared_ptr < Pollable > lookupByNameShared (const std::string& label);
   static void requestPollFDRegen();
   static int poll(int timeout); // do one round of polling; return 0 on okay; errno otherwise
   static void setControlSocket(string label);
